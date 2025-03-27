2fd0d1749a93bcb44fd3afacf9b489a0fbc0a975
mrinal1014 <131162988+mrinal1014@users.noreply.github.com>
2025-03-28 05:09:23 +0530

    Add files via upload

---
+++ student_management/students/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import student_list, add_student, student_search_by_registration,chatbot_page,home
+from django.urls import path
+from .views import chatbot_page, search_api
+urlpatterns = [
+    path("chatbot/", chatbot_page, name="chatbot"),
+    path('',home, name='home'),  # Home Page
+    path("about/", student_list, name="student_list"),
+    path("add/", add_student, name="add_student"),
+    
+]
+
+
